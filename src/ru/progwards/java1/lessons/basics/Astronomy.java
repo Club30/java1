77622a3e6f4647ba65de96bab82539296ea8d6b8
club30 <rusremont78@gmail.com>
2021-09-25 00:31:12 +0300

    H3 HomeWork

--- src/ru/progwards/java1/lessons/basics/Astronomy.java
+++ src/ru/progwards/java1/lessons/basics/Astronomy.java
@@ -8,19 +8,19 @@ public class Astronomy {
     static final double JUPITER_RADIUS = 71492;
 
     public static Double sphereSquare(Double r) {
-        return 4*PI*r*r;
+        return 4 * PI * r * r;
     }
 
     public static Double earthSquare() {
-        return 4*PI*EARTH_RADIUS*EARTH_RADIUS;
+        return 4 * PI * EARTH_RADIUS * EARTH_RADIUS;
     }
 
     public static Double mercurySquare() {
-        return 4*PI*MERCURY_RADIUS*MERCURY_RADIUS;
+        return 4 * PI * MERCURY_RADIUS * MERCURY_RADIUS;
     }
 
     public static Double jupiterSquare() {
-        return 4*PI*JUPITER_RADIUS*JUPITER_RADIUS;
+        return 4 * PI * JUPITER_RADIUS * JUPITER_RADIUS;
     }
 
     public static Double earthVsMercury() {
@@ -31,6 +31,16 @@ public class Astronomy {
         return earthSquare() - jupiterSquare();
     }
 
+    static int addAsStrings(int n1, int n2) {
+        return Integer.parseInt(Integer.toString(n1) + Integer.toString(n2));
+    }
+
+    public static void main(String[] args) {
+        System.out.println(addAsStrings(1, 2));
+    }
+}
+
+
 //    public static void main(String[] args) {
 //        System.out.println(earthSquare());
 //        System.out.println(mercurySquare());
@@ -38,4 +48,3 @@ public class Astronomy {
 //        System.out.println(earthVsMercury());
 //        System.out.println(earthVsJupiter());
 //    }
-}
